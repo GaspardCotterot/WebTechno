7fda2cb1bb04b4c9af4e4c46a2eb6835cf0d8c2f
GaspardCotterot <57559923+GaspardCotterot@users.noreply.github.com>
2021-05-07 09:26:10 +0200

    first commit
    //

---
+++ src/main/java/Isep/webtechno/WebtechnoApplication.java
@@ -0,0 +1,13 @@
+package Isep.webtechno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Isep/webtechno/WebtechnoApplicationTests.java
@@ -0,0 +1,13 @@
+package Isep.webtechno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
