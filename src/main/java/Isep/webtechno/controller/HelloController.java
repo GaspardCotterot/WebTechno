5599f771cd50ad9603a9263b6902cce5a9084730
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-09 22:24:50 +0200

    hellocontroller (pour test)

---
+++ src/main/java/Isep/webtechno/controller/HelloController.java
@@ -0,0 +1,26 @@
+package Isep.webtechno.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(path = "/")
+    private String hello() {
+        System.out.println("Hello");
+        return "Hello";
+    }
+
+    @PostMapping(path = "/")
+    private String helloPost() {
+        System.out.println("Hello POST");
+        return "Hello (POST)";
+    }
+
+    @GetMapping(path = "/admin")
+    private String admin() {
+        return "Welcome admin";
+    }
+}
