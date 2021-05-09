f8cb9c624dc1c0c412a90e969e01c8af9b31f1a2
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-09 22:47:28 +0200

    add findByMail to userRepo

--- src/main/java/Isep/webtechno/model/repo/UserRepository.java
+++ src/main/java/Isep/webtechno/model/repo/UserRepository.java
@@ -2,6 +2,9 @@ package Isep.webtechno.model.repo;
 
 import Isep.webtechno.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+    public User findByMail(String Mail);
 }
