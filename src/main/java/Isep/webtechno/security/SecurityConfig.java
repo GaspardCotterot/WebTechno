d1066e629711a6e8a3637a4b4c1df5340087b783
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-09 23:44:03 +0200

    Auth based on users in db

--- src/main/java/Isep/webtechno/security/SecurityConfig.java
+++ src/main/java/Isep/webtechno/security/SecurityConfig.java
@@ -1,57 +1,71 @@
 package Isep.webtechno.security;
 
+import Isep.webtechno.security.auth.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
     private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
     @Autowired
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
+    public SecurityConfig(PasswordEncoder passwordEncoder, UserService userService) {
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-//                .httpBasic()
-//                .and()
                 .authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/auth").authenticated()
                 .antMatchers("/**").permitAll() //allow all requests
-
                 .anyRequest()
                 .authenticated()
+                .and()
+                .formLogin()
                 ;
     }
 
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails adminUser = User.builder()
+//                .username("mat")
+//                .password(passwordEncoder.encode("pass"))
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails user = User.builder()
+//                .username("random")
+//                .password(passwordEncoder.encode("pass"))
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(adminUser, user);
+//    }
+
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails adminUser = User.builder()
-                .username("mat")
-                .password(passwordEncoder.encode("pass"))
-                .roles("ADMIN")
-                .build();
-        UserDetails user = User.builder()
-                .username("random")
-                .password(passwordEncoder.encode("pass"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(adminUser);
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(userService);
+
+        return daoAuthenticationProvider;
     }
 }
