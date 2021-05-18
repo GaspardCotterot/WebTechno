1ae92b89c3e131e44855f89ec22fe4211b226ed6
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-18 18:46:15 +0200

    id auto genere + add default admin

--- src/main/java/Isep/webtechno/model/entity/User.java
+++ src/main/java/Isep/webtechno/model/entity/User.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Data
 public class User implements UserDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
