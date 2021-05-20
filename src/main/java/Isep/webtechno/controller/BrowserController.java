ad8259015427218cc60d2174039a572809499adf
Lwii-U <louis.develter@gmail.com>
2021-05-20 11:56:38 +0200

    Add functions to check the user input

--- src/main/java/Isep/webtechno/controller/BrowserController.java
+++ src/main/java/Isep/webtechno/controller/BrowserController.java
@@ -1,2 +1,23 @@
-package Isep.webtechno.controller;public class BrowserController {
+package Isep.webtechno.controller;
+
+import Isep.webtechno.model.repo.HouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class BrowserController {
+
+    @Autowired
+    private HouseRepository houseRepository;
+
+
+    private boolean checkDatePattern(String date) { // Checks if a given date respects the pattern YYYY-MM-DD
+        return date.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+    }
+
+    private boolean isDateFuture(LocalDate firstDate, LocalDate secondDate) { // Checks the order of two given dates
+        return secondDate.isAfter(firstDate);
+    }
 }
