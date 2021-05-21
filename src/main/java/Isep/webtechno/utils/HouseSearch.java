8baf4bff1c62781a02aca4b32a9b28eea636aebd
Lwii-U <louis.develter@gmail.com>
2021-05-21 15:35:31 +0200

    Add HouseSearch object to reflect house search form

---
+++ src/main/java/Isep/webtechno/utils/HouseSearch.java
@@ -0,0 +1,31 @@
+package Isep.webtechno.utils;
+
+public class HouseSearch {
+    private String location;
+    private String arrival;
+    private String departure;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+}
