6b48589ddae028d76db80ed33d833991c3546063
Lwii-U <louis.develter@gmail.com>
2021-05-21 15:36:14 +0200

    Commit avant modifs dependances

--- src/main/java/Isep/webtechno/controller/BrowserController.java
+++ src/main/java/Isep/webtechno/controller/BrowserController.java
@@ -1,10 +1,17 @@
 package Isep.webtechno.controller;
 
+import Isep.webtechno.model.entity.House;
 import Isep.webtechno.model.repo.HouseRepository;
+import Isep.webtechno.utils.HouseSearch;
+import Isep.webtechno.utils.HouseSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class BrowserController {
@@ -12,6 +19,13 @@ public class BrowserController {
     @Autowired
     private HouseRepository houseRepository;
 
+    @PostMapping(path = "/search")
+    public List<House> findByLocation(@ModelAttribute HouseSearch search) {
+        // TODO: implement date checking
+        HouseSpecification spec = new HouseSpecification(search);
+
+        return null;
+    }
 
     private boolean checkDatePattern(String date) { // Checks if a given date respects the pattern YYYY-MM-DD
         return date.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
--- src/main/java/Isep/webtechno/utils/HouseSpecification.java
+++ src/main/java/Isep/webtechno/utils/HouseSpecification.java
@@ -7,18 +7,24 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HouseSpecification implements Specification<House> {
 
-    private final House criteria;
+    private final HouseSearch criteria;
 
-    public HouseSpecification(House criteria) {
+    public HouseSpecification(HouseSearch criteria) {
         this.criteria = criteria;
     }
 
     @Override
     public Predicate toPredicate(Root<House> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-
+//        final List<Predicate> predicates = new ArrayList<>();
+//        if (criteria.getLocation() != null) {
+//            predicates.add(criteriaBuilder.equal(root.get(House_.city), criteria.getLocation()));
+//        }
+//        return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
         return null;
     }
 }
