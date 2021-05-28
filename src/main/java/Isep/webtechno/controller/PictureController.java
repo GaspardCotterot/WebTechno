4eff7adf3f39b97958fb1e8ff940329f2434361f
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-29 00:01:54 +0200

    image ipload and display 1st working

--- src/main/java/Isep/webtechno/controller/PictureController.java
+++ src/main/java/Isep/webtechno/controller/PictureController.java
@@ -5,23 +5,35 @@ import Isep.webtechno.model.entity.Picture;
 import Isep.webtechno.model.entity.User;
 import Isep.webtechno.model.repo.HouseRepository;
 import Isep.webtechno.model.repo.PictureRepository;
+import Isep.webtechno.utils.FileUploadUtil;
 import Isep.webtechno.utils.GeneralService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(path = "/picture")
 public class PictureController {
+    private final PictureRepository pictureRepository;
+    private final HouseRepository houseRepository;
+    private final GeneralService generalService;
+
     @Autowired
-    PictureRepository pictureRepository;
-    @Autowired
-    HouseRepository houseRepository;
-    @Autowired
-    GeneralService generalService;
+    public PictureController(PictureRepository pictureRepository, HouseRepository houseRepository, GeneralService generalService) {
+        this.pictureRepository = pictureRepository;
+        this.houseRepository = houseRepository;
+        this.generalService = generalService;
+    }
 
     @PostMapping(path = "/add-or-edit/{houseId}")
     private ResponseEntity<String> addOrEdit(@PathVariable Integer houseId,
@@ -55,4 +67,29 @@ public class PictureController {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @PostMapping(path = "/upload")
+    private ResponseEntity<String> upload(@RequestParam("picture") MultipartFile multipartFile) throws IOException {
+//        User user = generalService.getUserFromContext();
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+//        Picture picture = user.getHouses().get(0).getPictures().get(0);
+//        picture.setUrl(fileName);
+//        pictureRepository.save(picture);
+
+//        String uploadDir = "photos/" + user.getHouses().get(0).getId();
+        FileUploadUtil.saveFile("photos/", fileName, multipartFile);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/files/{filename:.+}", produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = FileUploadUtil.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+
+
 }
