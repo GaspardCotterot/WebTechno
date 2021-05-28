830fe89f8389e8af810834ce05226cc850703043
Pierre-Louis Guillou <plguillou@gmail.com>
2021-05-28 10:52:05 +0200

    link to front (modif profile)

--- src/main/java/Isep/webtechno/controller/UserController.java
+++ src/main/java/Isep/webtechno/controller/UserController.java
@@ -4,11 +4,11 @@ import Isep.webtechno.model.converter.UserConverter;
 import Isep.webtechno.model.dto.BasicUserDto;
 import Isep.webtechno.model.entity.House;
 import Isep.webtechno.model.entity.User;
+import Isep.webtechno.model.repo.UserRepository;
 import Isep.webtechno.utils.GeneralService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +16,8 @@ import java.util.List;
 @RequestMapping(path = "/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     private GeneralService generalService;
     @Autowired
@@ -36,43 +38,55 @@ public class UserController {
         return getUser().getHouses();
     }
 
+
+    @PostMapping(path = "/modify-profile")
+    String modifyUserMail(@RequestParam String name, @RequestParam String mail) {
+        System.out.println("\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!\n!" + name + " " + mail);
+        User user = getUser();
+        user.setName(name);
+        user.setMail(mail);
+        userRepository.save(user);
+        return "Changed";
+    }
+
     //todo
-//    @PostMapping(path = "/{user_id}/add-booking")
-//    String addBookingToUser(@RequestBody Booking booking, @PathVariable int user_id) {
-//        User user = getUserById(user_id);
-//        if (user != null) {
-//            user.getBookings().add(booking);
-//            userRepository.save(user);
-//            return "Changed";
-//        } else {
-//            return "Error, no user with this id";
-//        }
-//    }
+    //    @PostMapping(path = "/{user_id}/add-booking")
+    //    String addBookingToUser(@RequestBody Booking booking, @PathVariable int user_id) {
+    //        User user = getUserById(user_id);
+    //        if (user != null) {
+    //            user.getBookings().add(booking);
+    //            userRepository.save(user);
+    //            return "Changed";
+    //        } else {
+    //            return "Error, no user with this id";
+    //        }
+    //    }
 
 
-//    @PostMapping(path = "/{user_id}/add-house")
-//    String addHouseToUser(@RequestBody House house, @PathVariable int user_id) {
-//        User user = getUserById(user_id);
-//        user.getHouses().add(house);
-//        userRepository.save(user);
-//        houseRepository.save(house);
-//        return "Changed";
-//    }
-//
-//    @PostMapping(path = "/{user_id}/modify-name")
-//    String modifyUserName(@RequestParam String name, @PathVariable int user_id) {
-//        User user = getUserById(user_id);
-//        user.setName(name);
-//        userRepository.save(user);
-//        return "Changed";
-//    }
-//
-//    @PostMapping(path = "/{user_id}/modify-mail")
-//    String modifyUserMail(@RequestParam String mail, @PathVariable int user_id) {
-//        User user = getUserById(user_id);
-//        user.setMail(mail);
-//        userRepository.save(user);
-//        return "Changed";
-//    }
+    //    @PostMapping(path = "/{user_id}/add-house")
+    //    String addHouseToUser(@RequestBody House house, @PathVariable int user_id) {
+    //        User user = getUserById(user_id);
+    //        user.getHouses().add(house);
+    //        userRepository.save(user);
+    //        houseRepository.save(house);
+    //        return "Changed";
+    //    }
+    //
+    //    @PostMapping(path = "/{user_id}/modify-name")
+    //    String modifyUserName(@RequestParam String name, @PathVariable int user_id) {
+    //        User user = getUserById(user_id);
+    //        user.setName(name);
+    //        userRepository.save(user);
+    //        return "Changed";
+    //    }
+    //
+    //    @PostMapping(path = "/{user_id}/modify-mail")
+    //    String modifyUserMail(@RequestParam String mail, @PathVariable int user_id) {
+    //        User user = getUserById(user_id);
+    //        user.setMail(mail);
+    //        userRepository.save(user);
+    //        return "Changed";
+    //    }
+
 
 }
