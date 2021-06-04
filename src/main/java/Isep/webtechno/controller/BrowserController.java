2416a599206ae0981e21c93dcca4967b5bb05019
LouisDvr <louis.develter@gmail.com>
2021-06-04 19:35:52 +0200

    Add date checking

--- src/main/java/Isep/webtechno/controller/BrowserController.java
+++ src/main/java/Isep/webtechno/controller/BrowserController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -19,16 +21,35 @@ public class BrowserController {
 
     @PostMapping(path = "/search")
     public List<House> findByLocation(@ModelAttribute HouseSearch search) {
-        // TODO: implement date checking
-        HouseSpecification spec = new HouseSpecification(search);
-        return houseRepository.findAll(spec);
+        System.out.println(search.getLocation()+" "+search.getArrival()+" "+search.getDeparture());
+        if (checkDatePattern(search.getArrival()) && checkDatePattern(search.getDeparture())) {
+            if (isDateAfter(stringToLocalDate(search.getArrival()), LocalDate.now())
+                    && isDateAfter(stringToLocalDate(search.getDeparture()), LocalDate.now())
+                    && isDateAfter(stringToLocalDate(search.getDeparture()), stringToLocalDate(search.getArrival()))) {
+                HouseSpecification spec = new HouseSpecification(search);
+                return houseRepository.findAll(spec);
+            }
+        }
+        return new ArrayList<>();
     }
 
     private boolean checkDatePattern(String date) { // Checks if a given date respects the pattern YYYY-MM-DD
         return date.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
     }
 
-    private boolean isDateFuture(LocalDate firstDate, LocalDate secondDate) { // Checks the order of two given dates
+    private LocalDate stringToLocalDate(String date) { // Convert a String of type YYYY-MM-DD to LocalDate object
+        String[] dateStrings = date.split("-");
+        int[] dateIntegers = new int[3];
+        try {
+            dateIntegers = Arrays.stream(dateStrings).mapToInt(Integer::parseInt).toArray();
+        } catch (Exception e) {
+            System.out.println("Got a problem converting the string date to LocalDate object !");
+            e.printStackTrace();
+        }
+        return LocalDate.of(dateIntegers[0], dateIntegers[1], dateIntegers[2]);
+    }
+
+    private boolean isDateAfter(LocalDate secondDate, LocalDate firstDate) { // Checks the order of two given dates
         return secondDate.isAfter(firstDate);
     }
 }
